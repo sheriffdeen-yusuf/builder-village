c0f6598cac61a1fb3e5e7fd076f5d8dc5926f572
Sheriffdeen Yusuf <sheriffdeenyusuf1130@gmail.com>
2023-01-18 12:29:04 +0100

    Added Email Activation Features for all User

--- controllers/emailActivateController.js
+++ controllers/emailActivateController.js
@@ -29,4 +29,56 @@ const clientAccountActivation = async (req, res) => {
   );
 };
 
-export { clientAccountActivation };
+const adminAccountActivation = async (req, res) => {
+  const { activatetoken } = req.body;
+  if (!activatetoken)
+    return res.status(400).send("Please Provide your Activation Token");
+
+  jwt.verify(
+    activatetoken,
+    process.env.ACCOUNT_ACTIVATION_TOKEN,
+    async (err, decoded) => {
+      if (err) return res.status(400).send("Incorrect or expired Link");
+      const admin = await Admin.findOne({ where: { email: decoded.email } });
+      if (admin) {
+        await Admin.update(
+          { verify: true },
+          { where: { email: decoded.email } }
+        );
+        return res
+          .status(200)
+          .send(`${decoded.username} Your account is now Activated`);
+      } else return res.status(401).send("Something went wrong, Try later!");
+    }
+  );
+};
+
+const vendorAccountActivation = async (req, res) => {
+  const { activatetoken } = req.body;
+  if (!activatetoken)
+    return res.status(400).send("Please Provide your Activation Token");
+
+  jwt.verify(
+    activatetoken,
+    process.env.ACCOUNT_ACTIVATION_TOKEN,
+    async (err, decoded) => {
+      if (err) return res.status(400).send("Incorrect or expired Link");
+      const vendor = await Vendor.findOne({ where: { email: decoded.email } });
+      if (vendor) {
+        await Vendor.update(
+          { verify: true },
+          { where: { email: decoded.email } }
+        );
+        return res
+          .status(200)
+          .send(`${decoded.username} Your account is now Activated`);
+      } else return res.status(401).send("Something went wrong, Try later!");
+    }
+  );
+};
+
+export {
+  clientAccountActivation,
+  adminAccountActivation,
+  vendorAccountActivation,
+};
--- routes/emailActivationRoutes.js
+++ routes/emailActivationRoutes.js
@@ -1,8 +1,14 @@
-import { clientAccountActivation } from "../controllers/emailActivateController.js";
+import {
+  clientAccountActivation,
+  adminAccountActivation,
+  vendorAccountActivation,
+} from "../controllers/emailActivateController.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/client", clientAccountActivation);
+router.post("/vendor", vendorAccountActivation);
+router.post("/admin", adminAccountActivation);
 
 export default router;
